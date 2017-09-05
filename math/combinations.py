d8282d3e5d214a0720599c341247570ea8800b27
abidkhan <abidkhan484@gmail.com>
2017-09-05 11:00:53 +0600

    5sep17
    i just learn about data structure or algorithms and math.... .....

---
+++ math/combinations.py
@@ -0,0 +1,12 @@
+def make_combination(n, k):
+    total = 1
+    for i in range(n, k, -1):
+        total *= i
+
+    another = 1;     
+    for i in range(1, n-k+1):
+        another *= i
+
+    return total//another
+
+print(make_combination(1000, 7))
